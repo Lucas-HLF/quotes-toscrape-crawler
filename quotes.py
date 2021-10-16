1f3b3bdd4b4eabb829976887d41024b7d78bfb1c
Lucas-HLF <lucashlfernandes33@gmail.com>
2021-10-16 15:37:32 -0300

    Salvando as informações em um arquivo json

--- quotes.py
+++ quotes.py
@@ -1,34 +1,44 @@
 from bs4 import BeautifulSoup
+import json
 import requests
 
-
-def find_quotes():
+def page_navegation():
     url = "https://quotes.toscrape.com/page/1/"
+    list_quotes = []   
     while True:
-        print(url)
+        print(f"Página {url[-3::].replace('/', '')}")
         source = requests.get(url).text
         soup = BeautifulSoup(source, 'lxml')
-
-        quotes_divs = soup.find_all("div", class_="quote")
-
-        for quote_div in quotes_divs:
-            phrase = quote_div.find('span', 'text').text
-            author = quote_div.find('small', 'author').text
-            tags = quote_div.find_all('a', 'tag')
-
-            print(dict({
-                "Phrase": phrase,
-                "Author": author,
-                "Tags": [tag.text for tag in tags]
-            }))
-
+        list_quotes.append(find_quotes(soup, list_quotes))
         try:
             url = f"https://quotes.toscrape.com{soup.find('li', 'next').a['href']}"
 
         except Exception:
-            print("Última página")
+            print(f"Última página, Página {url[-3::].replace('/', '')}")
             break
+
+    with open("quote.json", "a", encoding="utf-8") as json_file:
+        json.dump(list_quotes, json_file)
+
+
+def format_phrase(phrase):
+    new_phrase = phrase.replace('“', "")
+    return new_phrase.replace('”', "")
+
+
+def find_quotes(soup, list_quotes):
+    quotes_divs = soup.find_all("div", class_="quote")
+    for quote_div in quotes_divs:
+        phrase = quote_div.find('span', 'text').text
+        author = quote_div.find('small', 'author').text
+        tags = quote_div.find_all('a', 'tag')
+        
+        list_quotes.append(dict({
+                "Phrase": format_phrase(phrase),
+                "Author": author,
+                "Tags": [tag.text for tag in tags]
+                }))
 
 
 if __name__ == "__main__":
-    find_quotes()
+    page_navegation()
